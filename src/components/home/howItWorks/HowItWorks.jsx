a6e634e908e307309c1a9aad509c7f1a832944e1
hyzmael88 <bartttt15@hotmail.com>
2024-06-07 22:02:32 -0600

    refactor: Update WasteEctomy component layout and styling

--- src/components/home/howItWorks/HowItWorks.jsx
+++ src/components/home/howItWorks/HowItWorks.jsx
@@ -20,18 +20,18 @@ function HowItWorks() {
       <div className="container mx-auto flex flex-wrap  md:flex-nowrap ">
         <div className="w-full flex flex-col lg:flex-row items-center justify-between py-12 px-4 lg:px-12 bg-white">
           <div className=" max-w-lg">
-            <h2 className="text-[53px] font-bold mb-4 text-center text-[#1B3954]">
+            <h2 className="text-[22px] lg:text-[53px] font-bold mb-4 text-center text-[#1B3954]">
               How it <span className="text-[#5587B6]">works</span>.
             </h2>
-            <p className="text-[#5587B6] mb-4 font-bold text-[23px] max-w-lg">
+            <p className="text-[#5587B6] mb-4 font-bold text-[14px] text-center lg:text-start lg:text-[23px] max-w-lg">
               Reducing the cost of protective gowns by up to 80% while ensuring health and safety may sound too good to be true. But it isn’t.
             </p>
-            <p className="mb-8 text-[21px] max-w-lg">
+            <p className="mb-8 text-[10px] lg:text-[21px] max-w-lg text-center lg:text-start">
               Our patent-pending, anti-viral, anti-bacterial fabric can be washed up to 50 times with conventional hospital system detergents and equipment without losing protective properties and prevents cross-contamination, enabling caregivers to dramatically reduce gown changes.
             </p>
           </div>
-          <div className="lg:w-1/2 flex justify-end items-center mt-8 lg:mt-0  ">
-          <div className="relative flex justify-end items-center  cursor-pointer"  onClick={handlePlayPauseVideo}>
+          <div className="lg:w-1/2 flex justify-center lg:justify-end items-center mt-8 lg:mt-0  ">
+          <div className="relative flex justify-center lg:justify-end items-center  cursor-pointer"  onClick={handlePlayPauseVideo}>
           <video
               ref={videoRef}
               src="/assets/video/video.mp4"
@@ -41,7 +41,7 @@ function HowItWorks() {
               onPlay={() => setIsPlaying(true)}
               controls={true}
             />
-          <div className={isPlaying? 'hidden':"absolute inset-0 flex items-center justify-center ml-20 "}>
+          <div className={isPlaying? 'hidden':"absolute inset-0 flex items-center justify-center lg:ml-20 mb-8 lg:mb-0 "}>
               <div className="bg-white bg-opacity-75 p-4 rounded-full cursor-pointer" onClick={handlePlayPauseVideo}>
                 <svg className="h-12 w-12 text-blue-600" fill="currentColor" viewBox="0 0 24 24">
                   {isPlaying ? (
