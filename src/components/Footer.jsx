b721e57b53ebc61be8b45667f12ad8d38907dcee
hyzmael88 <bartttt15@hotmail.com>
2024-06-07 23:53:17 -0600

    refactor: Update WasteEctomy component layout and styling
    The WasteEctomy component layout and styling are updated to improve the visual presentation and responsiveness. Changes include adjusting the alignment of text and elements, updating font sizes, and optimizing the use of space. This refactor also includes using lazy loading for the image in the component.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,11 +5,11 @@ function Footer() {
         <footer className="bg-[#1B3954] text-white py-6">
           <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
             <div className="flex flex-col lg:flex-row justify-between items-center">
-              <div className="flex items-center space-x-4">
+              <div className="flex  items-center space-x-4">
                 <img className="h-8 w-auto" src="/assets/navbar/logo.png" alt="Medu Logo" />
                 
               </div>
-              <div className="flex space-x-8 mt-4 lg:mt-0 items-center">
+              <div className="flex flex-col lg:flex-row space-y-4 lg:space-y-0  lg:space-x-8 mt-4 lg:mt-0 items-center">
                 <a href="#" className="hover:text-gray-400">Our Products</a>
                 <a href="#" className="hover:text-gray-400">How it works</a>
                 <a href="#" className="hover:text-gray-400">Industries</a>
@@ -20,12 +20,20 @@ function Footer() {
                 </button>
               </div>
             </div>
-            <div className="border-t border-[#5587B6] mt-6 pt-4 flex flex-col lg:flex-row justify-between items-center">
-              <p className="text-sm text-gray-400">&copy; 2024 Medu Protection</p>
-              <div className="flex space-x-4 mt-4 lg:mt-0">
+            <div className="border-t border-[#82BE62] lg:border-[#5587B6] mt-6 pt-4 flex flex-col-reverse lg:flex-row justify-between items-center text-center lg:text-start">
+              <p className="text-sm text-gray-400 mt-4 lg:mt-0">&copy; 2024 Medu Protection</p>
+              
+              <div className="flex flex-col lg:flex-row space-y-4 lg:space-y-0 lg:space-x-4 mt-4 lg:mt-0">
                 <a href="#" className="text-sm text-gray-400 hover:text-gray-300">Terms</a>
                 <a href="#" className="text-sm text-gray-400 hover:text-gray-300">Privacy</a>
                 <a href="#" className="text-sm text-gray-400 hover:text-gray-300">Cookies</a>
+              </div>
+              <div className='lg:hidden flex justify-center gap-[55px] my-2'>
+                <img src="/assets/footer/fb.svg" alt="Facebook" className="" />
+                <img src="/assets/footer/ig.svg" alt="Facebook" className="" />
+                <img src="/assets/footer/phone.svg" alt="Facebook" className="" />
+                <img src="/assets/footer/email.svg" alt="Facebook" className="" />
+
               </div>
             </div>
           </div>
--- src/components/home/difference/Difference.jsx
+++ src/components/home/difference/Difference.jsx
@@ -3,18 +3,18 @@ import React from 'react'
 function Difference() {
     return (
         <div
-          className="relative flex flex-col items-center justify-center py-20 px-4 bg-cover bg-center text-white"
+          className="relative flex flex-col items-center justify-center py-4 lg:py-20 px-4 bg-cover bg-center text-white"
           style={{ backgroundImage: "url('/assets/difference/bg.jpg')" }}
         >
           <div className="absolute inset-0 bg-black opacity-50"></div>
           <div className="relative z-10 text-center flex flex-col items-center">
-            <h2 className="text-[50px] font-bold mb-4">
+            <h2 className="text-[16px] lg:text-[50px] font-bold mb-4">
               The difference that <span className="text-[#95BC67]">makes a difference.</span>
             </h2>
-            <p className="mb-8 max-w-2xl text-[20px]">
+            <p className="mb-8 max-w-2xl text-[7px]  lg:text-[20px] px-10 lg:px-0">
               Medu is not just the first of its kind. It’s the first of its kind offering a host of benefits to optimize savings of all kinds. See how Medu stacks up.
             </p>
-            <button className="bg-[#95BC67] text-white py-4 px-8 rounded-full hover:bg-green-600 transition text-[28px]">
+            <button className="bg-[#95BC67] text-white py-4 px-8 rounded-full hover:bg-green-600 transition text-[14px] lg:text-[28px]">
               Why MEDU
             </button>
           </div>
