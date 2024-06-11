1375d0068d4a92f88bc209eded8e4e0e0e5f89a3
hyzmael88 <31554724+hyzmael88@users.noreply.github.com>
2024-06-10 22:28:52 -0600

    refactor: Update WasteEctomy component layout and styling
    Improve visual presentation and responsiveness by adjusting alignment, updating font sizes, optimizing space, and using lazy loading for the image.

--- src/components/home/hero/Bar.jsx
+++ src/components/home/hero/Bar.jsx
@@ -3,14 +3,14 @@ import React from 'react'
 function Bar() {
   
     return (
-        <div className='absolute w-[252px] h-[217px] flex flex-col   justify-center items-center text-[#1B3954] bg-[rgba(242,247,250,0.50)] shadow-md backdrop-blur-[2px] bottom-0   right-20 rounded-[20px] text-left'>
-            <h3 className='font-bold text-white text-[14px]  '>
+        <div className='absolute w-[159px] lg:w-[252px] h-[137px] lg:h-[217px] flex flex-col   justify-center items-left text-[#1B3954] bg-[rgba(242,247,250,0.50)] shadow-md backdrop-blur-[2px] -bottom-4 lg:bottom-0 left-14  lg:right-20 rounded-[20px] px-4'>
+            <h3 className='font-bold text-white text-[9px] lg:text-[14px] text-left'>
             Your monthly savings  
             </h3>
-            <p className='text-[9px] leading-[13px] text-white '>
+            <p className='text-[6px] lg:text-[9px] leading-[13px] text-white '>
             Chart shows savings on your investment for last month
             </p>
-            <img src='/assets/hero/bars.png' alt='bar' className='w-[200px] h-[100px] object-cover '/>
+            <img src='/assets/hero/bars.png' alt='bar' className='w-[120px] h-[70px] lg:w-[200px] lg:h-[100px] object-cover '/>
             </div>
       )
   
--- src/components/home/hero/Savings.jsx
+++ src/components/home/hero/Savings.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 function Savings() {
   return (
-    <div className='absolute w-[233px] h-[92px] flex  justify-center items-center text-[#1B3954] bg-[rgba(242,247,250,0.50)] shadow-md backdrop-blur-[2px] top-16   right-0 rounded-[20px]'>
-        <p className='text-[#1B3954] font-bold text-[16px] leading-[15px] '>
-            up to <span className='text-[55px]'>80% </span><br/> cost Savings
+    <div className='absolute w-[152px] h-[60px] lg:w-[233px] lg:h-[92px] flex  justify-center items-center text-[#1B3954] bg-[rgba(242,247,250,0.50)] shadow-md backdrop-blur-[2px] top-32 lg:top-16   right-0 rounded-[20px]'>
+        <p className='text-[#1B3954] font-bold text-[10px] lg:text-[16px] leading-[15px] '>
+            up to <span className='text-[36px] lg:text-[55px]'>80% </span><br/> cost Savings
         </p>
         </div>
   )
