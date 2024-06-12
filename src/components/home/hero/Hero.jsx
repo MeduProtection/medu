aa34361930f055cfdc4dd02fdc9247df32f3ac59
hyzmael88 <31554724+hyzmael88@users.noreply.github.com>
2024-06-11 21:27:15 -0600

    refactor: Improve visual presentation and responsiveness of Hero component
    Update Hero component to use lazy loading for the image and adjust the paragraph element's max-width on medium and large screens. Enhances visual presentation and responsiveness, ensuring consistent styling across devices.

--- src/components/home/hero/Hero.jsx
+++ src/components/home/hero/Hero.jsx
@@ -9,18 +9,18 @@ function Hero() {
     <header className=" lg:py-24">
         <div className="container mx-auto flex flex-col lg:flex-row items-center">
           <div className="w-full flex flex-col justify-center items-center lg:items-start text-center lg:text-start px-4">
-            <h1 className="text-[28px] md:text-[45px] lg:text-[55px] font-bold leading-tight text-[#1B3954]  mb-4">
-              <span className='text-[#95BC67]  text-[17px] md:text-[27px] lg:text-[34px]'>
+            <h1 className="text-[28px] md:text-[45px] xl:text-[55px] font-bold leading-tight text-[#1B3954]  mb-4">
+              <span className='text-[#95BC67]  text-[17px] md:text-[27px] xl:text-[34px]'>
                 Save $150k per day</span> <br/> with the world&apos;s <br/> first Level 4 antiviral <br/> <span className='text-[#4E88B0]'>reusable gown</span>.
             </h1>
-            <p className="max-w-sm md:max-w-md  lg:max-w-full mb-8 md:mb-4 text-gray-700 text-[10px] md:text-[17px] lg:text-[21px]">
+            <p className="max-w-sm md:max-w-md  lg:max-w-full mb-8 md:mb-4 text-gray-700 text-[10px] md:text-[17px] xl:text-[21px]">
               You save lives. Now you can save the planet, along with tens of millions of dollars every month with the
               only washable, sustainable medical gown of its kind.
             </p>
-            <p className="max-w-sm lg:max-w-full mb-8 font-bold text-[#5587B6] text-[10px] md:text-[17px] lg:text-[21px]">
+            <p className="max-w-sm lg:max-w-full mb-8 font-bold text-[#5587B6] text-[10px] md:text-[17px] xl:text-[21px]">
             See how our revolutionary technology is transforming healthcare.
             </p>
-            <button className="text-white bg-[#95BC67] hover:bg-[#059669]  focus:ring-[#059669]  rounded-[40px] text-[14px] md:text-[22px] lg:text-[28px] px-5 py-2.5 text-center lg:w-3/4">
+            <button className="text-white bg-[#95BC67] hover:bg-[#059669]  focus:ring-[#059669]  rounded-[40px] text-[14px] md:text-[22px] xl:text-[28px] px-5 py-2.5 text-center lg:w-3/4">
               Get Started Now
             </button>
             
