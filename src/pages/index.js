0a9afae74346a82d9f802b99a50dea2d3df420cf
hyzmael88 <31554724+hyzmael88@users.noreply.github.com>
2024-06-18 02:11:37 -0600

    refactor: Improve visual presentation and responsiveness of Home page
    Apply lazy loading for images and add meta tags for SEO optimization. These changes enhance the visual presentation and responsiveness of the Home page, ensuring consistent styling and improving the user experience across devices.

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,11 +11,27 @@ import Difference from "@/components/home/difference/Difference";
 import CallToAction from "@/components/home/callToAction/CallToAction";
 import Form from "@/components/home/form/Form";
 import Hero from "@/components/home/hero/Hero";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
+    <>
+    <Head>
+  <meta name="theme-color" content="#050409"/>
+  <title>MEDU Protection.</title>
+  <meta name="description" content="At MEDU Protection, we are committed to advancing healthcare safety with our state-of-the-art medical gowns and protective wear. Each piece is engineered for durability, offering up to 50 wash cycles without losing protective properties. This ensures that healthcare professionals can maintain the highest standards of hygiene and safety, repeatedly and cost-effectively." />
+  <meta name="keywords" content="Medical gowns, Washable hospital gowns, Fluid-resistant gowns, ppe, anti cross-contamination, washable coverall, sustainable gowns, personal protective equipment" />
+  <meta property="og:title" content="MEDU Protection" />
+  <meta property="og:description" content="At MEDU Protection, we are committed to advancing healthcare safety with our state-of-the-art medical gowns and protective wear. Each piece is engineered for durability, offering up to 50 wash cycles without losing protective properties. This ensures that healthcare professionals can maintain the highest standards of hygiene and safety, repeatedly and cost-effectively." />
+  <meta property="og:image" content="/assetsSecurity/ogimage.jpg" />
+  <meta property="og:url" content="https://www.meduprotection.com" />
+  <meta name="twitter:card" content="summary_large_image" />
+  <meta name="twitter:image" content="/assetsSecurity/ogimage.jpg" />
+  <link rel="icon" href="favicon.ico" />
+</Head>
+
     <div className="overflow-hidden">
       <Hero/>
       <div className="relative ">
@@ -38,5 +54,6 @@ export default function Home() {
       <Difference/>
       <CallToAction/>
     </div>
+    </>
   );
 }
