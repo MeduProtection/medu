073fb794a32400927d6bd3e780ee208723540473
Diego <diego.hurtado.olivares@gmail.com>
2025-11-24 21:55:02 -0600

    redirects

--- next.config.mjs
+++ next.config.mjs
@@ -4,40 +4,40 @@ const nextConfig = {
     domains: ['cdn.sanity.io'],
   },
   reactStrictMode: true,
-  // async redirects() {
-  //   return [
-  //     {
-  //       source: '/OurProducts',
-  //       destination: '/',
-  //       permanent: false,
-  //     },
-  //     {
-  //       source: '/HowItWorks',
-  //       destination: '/',
-  //       permanent: false,
-  //     },
-  //     {
-  //       source: '/Industries',
-  //       destination: '/',
-  //       permanent: false,
-  //     },
-  //     {
-  //       source: '/AboutUs',
-  //       destination: '/',
-  //       permanent: false,
-  //     },
-  //     {
-  //       source: '/Blog',
-  //       destination: '/',
-  //       permanent: false,
-  //     },
-  //     {
-  //       source: '/ContactUs',
-  //       destination: '/',
-  //       permanent: false,
-  //     },
-  //   ];
-  // },
+  async redirects() {
+    return [
+      {
+        source: '/OurProducts',
+        destination: '/',
+        permanent: false,
+      },
+      {
+        source: '/HowItWorks',
+        destination: '/',
+        permanent: false,
+      },
+      {
+        source: '/Industries',
+        destination: '/',
+        permanent: false,
+      },
+      {
+        source: '/AboutUs',
+        destination: '/',
+        permanent: false,
+      },
+      {
+        source: '/Blog',
+        destination: '/',
+        permanent: false,
+      },
+      {
+        source: '/ContactUs',
+        destination: '/',
+        permanent: false,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
